4c368522aac8fda6298db0d3a163a70df378ffe0
Arnaud (Limayrac) <arnaud.rabhi@limayrac.fr>
2023-07-05 15:46:04 +0200

    Wip login/logout

--- mini_projet_django/urls.py
+++ mini_projet_django/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mini_projet_equipement.views import login_view, logout_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
