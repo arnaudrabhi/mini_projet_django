e463363e532b2627515b5636fc1c473bca9d4752
Arnaud (Limayrac) <arnaud.rabhi@limayrac.fr>
2023-08-15 22:52:57 +0200

    WIP derniere modifications

--- mini_projet_equipement/models.py
+++ mini_projet_equipement/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+
+class Teacher(AbstractUser):
+
+
+class Equipment(models.Model):
+    name = models.CharField(
+        max_length=126,
+        null=False,
+        help_text="Nom de l'équipement",
+    )
+
